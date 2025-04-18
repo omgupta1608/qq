06c8a8bcdc632012a140cdc0fa5b6a993419deab
Om Gupta <omgupta1608@gmail.com>
2025-04-18 12:14:21 +0530

    add: config management with confy

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,60 @@
+use std::error::Error;
+
 use clap::{Parser, Subcommand};
+use confy;
+use serde::{Deserialize, Serialize};
+const APP_NAME: &str = "qq";
 
 #[derive(Parser)]
-#[command(name = "qq")]
-#[command(about = "qq", version = "1.0")]
+#[command(name = APP_NAME)]
+#[command(about = APP_NAME, version = "1.0")]
 struct Cli {
     #[command(subcommand)]
-    command: Command,
+    command: Option<Command>,
 }
 
 #[derive(Subcommand)]
 enum Command {
     About,
+    Add { item: i32 },
+    Remove,
 }
 
-fn main() {
-    let cli = Cli::parse();
-    match cli.command {
-        Command::About => {
-            println!("qq cli")
-        }
+#[derive(Default, Serialize, Deserialize, Debug)]
+struct Config {
+    version: String,
+    items: Vec<i32>,
+}
+
+impl Config {
+    fn add(&mut self, item: i32) {
+        self.items.push(item);
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
+    let mut current_cfg: Config = confy::load(APP_NAME, None)?;
+    match cli.command {
+        // default - lists items
+        None => {
+            println!("{:?}", current_cfg.items)
+        }
+        // executes subcommands
+        Some(command) => match command {
+            Command::About => {
+                let file = confy::get_configuration_file_path(APP_NAME, None)?;
+                println!("The qq config file path is: {:#?}", file);
+            }
+            Command::Add { item } => {
+                current_cfg.add(item);
+                confy::store(APP_NAME, None, current_cfg)?;
+            }
+            Command::Remove => {
+                println!("{APP_NAME} remove")
+            }
+        },
+    }
+    Ok(())
 }
